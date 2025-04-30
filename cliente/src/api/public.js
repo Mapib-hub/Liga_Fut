eede8450a8df3a5d983381feb9059103189b5d52
Mapib-hub <mapibpublicidad@gmail.com>
2025-04-30 12:31:38 -0400

    Front terminado con axios arreglado para produccion

--- cliente/src/api/public.js
+++ cliente/src/api/public.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4000/public',
-    //baseURL: '/public',
+    //baseURL: 'http://localhost:4000/public',
+    baseURL: '/public',
     withCredentials: true,
 });
 
